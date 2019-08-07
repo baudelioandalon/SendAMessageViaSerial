71d2c0f432e544b9390e0070e1d6511ecca7f7a9
baudelioandalon <baudelio_andalon@hotmail.com>
2019-08-07 16:07:30 -0500

    #Works

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
 
+String mensaje = "";
+
+int verificado = 0;
+String Temp = "";
+String om = "";
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+   while(Serial.available() > 0){
+    
+    char recibido = Serial.read();
+    om = recibido;
+    mensaje = Temp + om;
+    Temp = mensaje;
+    mensaje = "";
+    Serial.println(Temp + " con tamaño de: " + Temp.length());
+    delay(50);
+   }
+   if(Temp.length() == 5){
+    Serial.println("ENTRO");
+    Temp = "";
+   }
+
+}//fin loop
